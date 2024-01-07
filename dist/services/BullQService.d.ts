11659c6e75b512f0e7ee4350e88c1780c780c0a8
Tomer <tom7ly@gmail.com>
2024-01-07 16:31:15 +0200

    Squashed 'scheduler-shared/' changes from 21bd881..0b27f9a
    0b27f9a cleanups
    
    git-subtree-dir: scheduler-shared
    git-subtree-split: 0b27f9aceab5f85eb13d13360057ab7c96b262c9

--- dist/services/BullQService.d.ts
+++
@@ -1,9 +0,0 @@
-import Bull, { Job, JobOptions } from 'bull';
-import { BQType } from '../models/BullQ.models';
-export interface IBullQService {
-    addTask<T>(data: T, options: JobOptions): Promise<Job>;
-    processTask(processor: (ITask: any) => any): Promise<void>;
-    getJobs<T>(): Promise<Job<T>[]>;
-    getJob<T>(jobId: Bull.JobId): Promise<Job<T>>;
-}
-export declare const initBullQService: (queueName: BQType) => IBullQService;
